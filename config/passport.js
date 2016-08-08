e883e6b31d1efaeef75855f5ef1ae98736746efc
Bien jerico Cueto <bien.jerico@gmail.com>
2016-08-08 19:29:10 +0800

    passport.js and server.js

--- config/passport.js
+++ config/passport.js
@@ -63,7 +63,8 @@ module.exports = function(passport) {
                 return done(err);
 
 		      if (rows.length) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                return done(null, false, {'signupMessage': 'That email is already taken.'});
+                // return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
             } else {
 
 				// if there is no user with that email
@@ -102,13 +103,15 @@ module.exports = function(passport) {
 			if (err)
                 return done(err);
 			 if (!rows.length) {
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, { "loginMessage":'No user found.'});
+                // return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
             } 
 			
 			// if the user is found but the password is wrong
             if (!( rows[0].password == password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-			
+			     return done(null, false, { "loginMessage": 'Oops! Wrong password.'}); 
+                // return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
             // all is well, return successful user
             return done(null, rows[0]);			
 		
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-var express = require('express');
-var app = express();
-var port     = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var flash = require('connect-flash');
+var express       = require('express');
+var app           = express();
+var port          = process.env.PORT || 3000;
+var bodyParser    = require('body-parser');
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var flash         = require('connect-flash');
 
 
+// middleware
 app.use(express.static(__dirname + "/public"));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
-
-
-
-// required for passport
+app.use(bodyParser.urlencoded({ extended: true }));// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());// parse application/json
 app.use(session({ secret: 'secretsecretsecret' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
@@ -29,39 +24,43 @@ require('./config/passport')(passport);
 app.post('/signup', passport.authenticate('local-signup', {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
+    passReqToCallback: true
+}),function(req, email, password, done) {
+  console.log(done);
+});
+
 
 
 // process the login form
 app.post('/login', passport.authenticate('local-login', {
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
+    passReqToCallback: true
+}),function(req, email, password, done) {
+  console.log(done);
+});
 
 // =====================================
 // LOGOUT ==============================
 // =====================================
 app.get('/logout', function(req, res) {
 	req.logout();
-	// res.sendFile('/login.html');
-});
-
-app.get('/profile', function (req, res) {
-  console.log(user);
-  res.send('welcome');
-});
-
-app.get('/login', function (req, res) {
-  res.sendFile('login.html');
-});
-
-app.get('/signup', function (req, res) {
-  res.sendFile('signup.html');
+	res.redirect('/login');
 });
 
 
+// api endpoints for login, content and logout
+app.get("/login", function (req, res) {
+
+    res.send("<p>Please login!</p><form method='post' action='/login'><input type='text' name='email'/><input type='password' name='password'/><button type='submit' value='submit'>Submit</buttom></form>");
+});
+
+
+app.get("/profile", function (req, res) {
+  console.log(req.isAuthenticated());
+  console.log(req.user.email);
+    res.send("Congratulations! you've successfully logged in.");
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
